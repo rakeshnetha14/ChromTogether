49c7e5c2036c85201dc69a4d60fbdb08d38d04d3
Rakesh Netha Vadnala <31205579+rakeshnetha14@users.noreply.github.com>
2020-01-27 07:59:13 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -6,14 +6,6 @@ read -p "Enter Chromatin interaction data file name : " file1
 read -p "Enter the directory containing chip-seq files" directory1
 read -p "Enter the number of randomizations" nr
 
-#file1_name=$(echo $name | cut -f 1 -d '.')
-#file1_extension=$(echo $name | cut -f 2 -d '.')
-#file2=
-
-#echo $file2
-
-#bedtools sort -i $file1 > $file1_name'_sort.'$file1_extension
-
 python chromatin_data_processing.py $file1
 python chipseq_data_processing.py $file1 $directory1
 python bipartite_randomization.py $file1 $nr
