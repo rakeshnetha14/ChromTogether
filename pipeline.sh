a24accf526bbe487d5992356f663cc1c04cf5cb8
Rakesh Netha Vadnala <31205579+rakeshnetha14@users.noreply.github.com>
2020-01-28 12:40:11 +0530

    Update pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -3,11 +3,14 @@
 # This is a pipeline to find the significantly attracting and repelling TF pairs in the spatial proximal regions of the chromatin data. Also, it contain also find the above TF pairs in sequentailly in the above considered regions of chromatin.
 
 read -p "Enter Chromatin interaction data file name : " file1
+read -p "Enter the genome sequence fasta file" file2
 read -p "Enter the directory containing chip-seq files" directory1
+read -p "Enter the directory containing pwms" directory2
 read -p "Enter the number of randomizations" nr
 
 python chromatin_data_processing.py $file1
 python chipseq_data_processing.py $file1 $directory1
+python motifsites.py $file1 $file2 $directory2
 python bipartite_randomization.py $file1 $nr
 
 
